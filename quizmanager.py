b55c33fbfc0f249fdf56e9b76960fa1aa1050025
Alan C Bonnici <chribonn@gmail.com>
2022-07-29 16:53:20 +0200

    upd

---
+++ quizmanager.py
@@ -0,0 +1,58 @@
+# QuizManager manages the quiz content
+import os.path
+import os
+import quizparser
+import datetime
+
+class QuizManager:
+    def __init__(self, quizfolder):
+        self.quizfolder = quizfolder
+        
+        # Most recently selected quiz
+        self.the_quiz = None
+        
+        # Collection of quizzes
+        self.quizzes = dict()
+        
+        # Store result of most recent quiz
+        self.results = None
+        
+        # Make sure that the quiz folder exists
+        self.quiztaker = ""
+        
+        # Build the list of quizzes
+        if (os.path.exists(self.quizfolder) == False):
+            raise FileNotFound("The quiz folder does not seem to exist")
+            
+        
+    def _build_quiz_list(self):
+        dircontents = os.scandir(self.quizfolder)
+        for i, f in enumerate(dircontents):
+            if f.is_file():
+                parser = quizparser.QuizParser()
+                self.quizzes[i+1] = parser.parse_quiz(f)
+                
+        
+    # Print a list of the current installed quizzes
+    def list_quizzes(self):
+        for k, v in self.quizzes.items():
+            print (f"({k}) {v.name}")
+            
+    # start the given quiz for the user and return the results
+    def take_quiz(self, quizif, username):
+        pass
+    
+    # prints the results of the most recently taken quiz
+    def print_results(self):
+        pass
+    
+    # save the results ofthe most recent quiz to a file
+    # the file is named using the current date as 
+    # QuizResults_YYYY_MM_DD_N (N is incremented until unique)
+    def save_results(self):
+        pass
+    
+    if __name__ == "__main__":
+        qm = QuizManager("Quizzes")
+        qm.list_quizzes()
+    
